c51ef99b455f8b126b3127c270b9bb05df1bcf94
RekGRpth <rekgrpth@gmail.com>
2020-05-15 10:03:53 +0500

    up

--- run.sh
+++ run.sh
@@ -15,9 +15,11 @@ docker run \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
+    --env XAUTHORITY=/home/.Xauthority \
     --hostname pgadmin3 \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly \
+    --mount type=bind,source=$HOME/.Xauthority,destination=/home/.Xauthority,readonly \
     --mount type=volume,source=pgadmin3,destination=/home \
     --name pgadmin3 \
     --network docker \
